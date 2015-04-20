2a7ab31ad153eb2295d693dbf80055268f00d91d
ms2sato <ms2.sato@gmail.com>
2015-04-20 22:33:19 +0900

    the first

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+if ENV['REDISTOGO_URL']
+  redis_uri = URI(ENV['REDISTOGO_URL'])
+  # http://higelog.brassworks.jp/?p=2307
+  Rails::Application.config.session_store :redis_store, expire_after: 60.days, :servers => {
+                                                           host: redis_uri.host,
+                                                           port: redis_uri.port,
+                                                           password: redis_uri.password,
+                                                           namespace: "sshare:sessions"
+                                                       }, expires_in: 60.days, :domain => '.infopeers.net'
+  # elsif Rails.env.development?
+else
+  Rails.application.config.session_store :cookie_store, key: '_sshare_session'
+end
