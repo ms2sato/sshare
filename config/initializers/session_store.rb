c72b1d3a77d0136ce39b08cfc8c90f2be229d30d
ms2sato <ms2.sato@gmail.com>
2015-04-21 04:47:17 +0900

    rm

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,12 +3,12 @@
 if ENV['REDISTOGO_URL']
   redis_uri = URI(ENV['REDISTOGO_URL'])
   # http://higelog.brassworks.jp/?p=2307
-  Sshare::Application.config.session_store :redis_store, expire_after: 60.days, :servers => {
+  Sshare::Application.config.session_store :redis_store, expire_after: 60.days, :domain => '.herokuapp.com', :servers => {
                                                            host: redis_uri.host,
                                                            port: redis_uri.port,
                                                            password: redis_uri.password,
                                                            namespace: "sshare:sessions", :domain => '.herokuapp.com'
-                                                       }, expires_in: 60.days, :domain => 'herokuapp.com'
+                                                       }, expires_in: 60.days
   # elsif Rails.env.development?
 else
   Sshare.application.config.session_store :cookie_store, key: '_sshare_session'
